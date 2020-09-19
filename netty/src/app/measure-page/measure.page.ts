db317bbc6594d56ad86352aa7ffb5167818f6c65
AdrianKrebs <kra@panter.ch>
2020-09-19 17:57:35 +0200

    some clean up

--- netty/src/app/measure-page/measure.page.ts
+++ netty/src/app/measure-page/measure.page.ts
@@ -43,14 +43,16 @@ export class MeasurePage implements OnInit {
 
     icon(transaction: Transaction) {
         switch (transaction.category) {
-            case 'transportation':
+            case 'Flight':
+                return 'jam jam-plane';
+            case 'Train':
                 return 'jam jam-train';
-            case 'fuel':
+            case 'Car':
                 return 'jam jam-car';
             case 'Food':
                 return 'jam jam-pizza-slice';
             case 'Goods':
-                return 'jam jam-gamepad';
+                return 'jam jam-shopping-cart';
             default:
                 return 'jam jam-credit-card';
         }
